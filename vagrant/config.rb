77531a38c25bcff9263544a55618a325bb2a1ed1
Christian Simon <simon@swine.de>
2016-04-11 14:34:21 +0100

    Disable shared folders

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -64,7 +64,7 @@ $expose_docker_tcp=2375
 # Enable NFS sharing of your home directory ($HOME) to CoreOS
 # It will be mounted at the same path in the VM as on the host.
 # Example: /Users/foobar -> /Users/foobar
-#$share_home=false
+$share_home=false
 
 # Customize VMs
 #$vm_gui = false
@@ -78,7 +78,7 @@ $vm_minion_cpus = 1
 # $shared_folders = {'/path/on/host' => '/path/on/guest', '/home/foo/app' => '/app'}
 # or, to map host folders to guest folders of the same name,
 # $shared_folders = Hash[*['/home/foo/app1', '/home/foo/app2'].map{|d| [d, d]}.flatten]
-$shared_folders = {'../apps' => '/apps'}
+#$shared_folders = {'../apps' => '/apps'}
 
 # Enable port forwarding from guest(s) to host machine, syntax is: { 80 => 8080 }, auto correction is enabled by default.
 #$forwarded_ports = {}
