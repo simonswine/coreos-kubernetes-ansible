932195a64423d0a2ca3ef8170e0a2c2fa3f8d56b
Christian Simon <simon@swine.de>
2016-04-11 14:25:21 +0100

    Upgrade cores os to latest stable

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -43,7 +43,7 @@ $instance_name_prefix="k8s"
 # For example, to deploy version 709.0.0, set $image_version="709.0.0".
 # The default value is "current", which points to the current version
 # of the selected channel
-$image_version = "681.2.0"
+$image_version = "899.15.0"
 
 # Official CoreOS channel from which updates should be downloaded
 $update_channel='stable'
